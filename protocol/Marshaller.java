bbbf88d4d6dc0fe4d224be27f1210e3a3b8f6630
Sean Y <seanyang@college.harvard.edu>
2023-02-10 15:07:42 -0500

    Make Message fields all required using a constructor and getters

--- protocol/Marshaller.java
+++ protocol/Marshaller.java
@@ -2,23 +2,22 @@ package protocol;
 
 public class Marshaller {
     public static byte[] marshall(Message message) {
-        byte[] marshalledMessage = new byte[message.characterLength];
-        marshalledMessage[0] = message.version;
-        marshalledMessage[1] = message.characterLength;
-        marshalledMessage[2] = message.operation;
-        marshalledMessage[3] = message.exception;
-        System.arraycopy(message.content, 0, marshalledMessage, 4, message.content.length);
+        byte[] marshalledMessage = new byte[message.getCharacterLength()];
+        marshalledMessage[0] = message.getVersion();
+        marshalledMessage[1] = message.getCharacterLength();
+        marshalledMessage[2] = message.getOperation();
+        marshalledMessage[3] = message.getException();
+        System.arraycopy(message.getContent(), 0, marshalledMessage, 4, message.getContent().length);
         return marshalledMessage;
     }
 
-    public static Message unmarshall(byte[] message) {
-        Message unmarshalledMessage = new Message();
-        unmarshalledMessage.version = message[0];
-        unmarshalledMessage.characterLength = message[1];
-        unmarshalledMessage.operation = message[2];
-        unmarshalledMessage.exception = message[3];
-        unmarshalledMessage.content = new byte[unmarshalledMessage.characterLength - 4];
-        System.arraycopy(message, 4, unmarshalledMessage.content, 0, unmarshalledMessage.content.length);
-        return unmarshalledMessage;
+    public static Message unmarshall(byte[] marshalledMessage) {
+        byte version = marshalledMessage[0];
+        byte characterLength = marshalledMessage[1];
+        byte operation = marshalledMessage[2];
+        byte exception = marshalledMessage[3];
+        byte[] content = new byte[characterLength - 4];
+        System.arraycopy(marshalledMessage, 4, content, 0, content.length);
+        return new Message(version, characterLength, operation, exception, content);
     }
 }
--- protocol/Message.java
+++ protocol/Message.java
@@ -1,9 +1,33 @@
 package protocol;
 
 public class Message {
-    public byte version;
-    public byte characterLength;
-    public byte operation;
-    public byte exception;
-    public byte[] content;
+    private byte version;
+    private byte characterLength;
+    private byte operation;
+    private byte exception;
+    private byte[] content;
+
+    public Message(byte version, byte characterLength, byte operation, byte exception, byte[] content) {
+        this.version = version;
+        this.characterLength = characterLength;
+        this.operation = operation;
+        this.exception = exception;
+        this.content = content;
+    }
+
+    public byte getVersion() {
+        return version;
+    }
+    public byte getCharacterLength() {
+        return characterLength;
+    }
+    public byte getOperation() {
+        return operation;
+    }
+    public byte getException() {
+        return exception;
+    }
+    public byte[] getContent() {
+        return content;
+    }
 }
