4f4cec876d6efa119a2908dc2c5ab5b64e45ffae
Sean Y <seanyang@college.harvard.edu>
2023-02-12 20:26:13 -0500

    Move byte utilities out to a class

---
+++ utility/ByteConverter.java
@@ -0,0 +1,52 @@
+package utility;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.UUID;
+
+public class ByteConverter {
+  public static String byteArrayToString(byte[] bytes) {
+    return new String(bytes);
+  }
+
+  public static byte[] stringToByteArray(String string) {
+    return string.getBytes();
+  }
+
+  public static byte[][] splitByteArray(byte[] array, byte separator) {
+      int separatorCount = 0;
+      for (byte b : array) {
+          if (b == separator) {
+              separatorCount++;
+          }
+      }
+
+      byte[][] result = new byte[separatorCount + 1][];
+      int index = 0;
+      int startIndex = 0;
+      for (int i = 0; i < array.length; i++) {
+          if (array[i] == separator) {
+              result[index] = Arrays.copyOfRange(array, startIndex, i);
+              startIndex = i + 1;
+              index++;
+          }
+      }
+      result[index] = Arrays.copyOfRange(array, startIndex, array.length);
+      return result;
+  }
+
+  public static UUID byteArrayToUUID(byte[] bytes) {
+      ByteBuffer bb = ByteBuffer.wrap(bytes);
+      long firstLong = bb.getLong();
+      long secondLong = bb.getLong();
+      return new UUID(firstLong, secondLong);
+  }
+
+  public static byte[] UUIDToByteArray(UUID uuid) {
+    ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+    bb.putLong(uuid.getMostSignificantBits());
+    bb.putLong(uuid.getLeastSignificantBits());
+    return bb.array();
+  }
+
+}
