2ab7d440c589339a64e9bc3547fda25bca6fb91a
Sean Y <seanyang@college.harvard.edu>
2023-02-14 18:54:22 -0500

    delete everything

--- build.sh
+++
@@ -1,5 +0,0 @@
-# compile everything including everything in the subdirectories
-for i in $(find . -name "*.java"); do
-    echo "Compiling $i"
-    javac $i
-done
--- test.sh
+++
@@ -1,4 +0,0 @@
-# run all the java .class files in tests but without the file extension
-for file in tests/*.class; do
-    java -ea ${file%.*}
-done
