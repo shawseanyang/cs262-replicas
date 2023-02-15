63ba539754b6a79b766fff2a383764af65d82809
Sean Y <seanyang@college.harvard.edu>
2023-02-14 21:55:16 -0500

    Create CommandParser

---
+++ src/main/java/com/chatapp/client/CommandParser.java
@@ -0,0 +1,46 @@
+package com.chatapp.client;
+
+import com.chatapp.client.commands.Command;
+import com.chatapp.client.commands.CreateAccountCommand;
+import com.chatapp.client.commands.DeleteAccountCommand;
+import com.chatapp.client.commands.ListAccountsCommand;
+import com.chatapp.client.commands.LogInCommand;
+import com.chatapp.client.commands.LogOutCommand;
+import com.chatapp.client.commands.SendMessageCommand;
+
+// Parses a single string representing a commandline command into a Command object
+
+// syntax: <command> <arg1> <arg2> ...
+
+public class CommandParser {
+  public static Command parse(String command) throws IllegalArgumentException {
+    try {
+      String[] parts = command.split(" ");
+      String commandName = parts[0];
+      String[] args = new String[parts.length - 1];
+      System.arraycopy(parts, 1, args, 0, args.length);
+      switch (commandName) {
+        case "create_account":
+          return new CreateAccountCommand(args[0]);
+        case "list_accounts":
+          return new ListAccountsCommand(args[0]);
+        case "logout":
+          return new LogOutCommand();
+        case "login":
+          return new LogInCommand(args[0]);
+        case "delete_account":
+          return new DeleteAccountCommand();
+        case "send":
+          return new SendMessageCommand(args[0], args[1]);
+        default:
+          throw new IllegalArgumentException("Unknown command: " + commandName);
+      }
+    } catch (Exception e) {
+      if (e instanceof IllegalArgumentException) {
+        throw e;
+      } else {
+        throw new IllegalArgumentException("Invalid command: " + command);
+      }
+    }
+  }
+}
---
+++ src/test/java/com/chatapp/client/CommandParserTest.java
@@ -0,0 +1,159 @@
+package com.chatapp.client;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.chatapp.client.commands.Command;
+import com.chatapp.client.commands.CreateAccountCommand;
+import com.chatapp.client.commands.DeleteAccountCommand;
+import com.chatapp.client.commands.ListAccountsCommand;
+import com.chatapp.client.commands.LogOutCommand;
+import com.chatapp.client.commands.SendMessageCommand;
+
+public class CommandParserTest {
+  @Test
+  public void parse_emptyCommand_ExceptionThrown() {
+    
+    try {
+      CommandParser.parse("");
+    } catch (Exception e) {
+      assertTrue(e instanceof IllegalArgumentException);
+    }
+  }
+
+  @Test
+  public void parse_invalidCommand_ExceptionThrown() {
+    
+    try {
+      CommandParser.parse("nonsense");
+    } catch (Exception e) {
+      assertTrue(e instanceof IllegalArgumentException);
+    }
+  }
+
+  @Test
+  public void parse_logOutCommand_createsLogOutCommand() {
+    
+    try {
+      Command command = CommandParser.parse("logout");
+      assertTrue(command instanceof LogOutCommand);
+    } catch (Exception e) {
+      assertTrue(false);
+    }
+  }
+
+  @Test
+  public void parse_logInCommand_createsLogInCommand() {
+    
+    try {
+      CommandParser.parse("login alan_turing");
+    } catch (Exception e) {
+      assertTrue(false);
+    }
+  }
+
+  @Test
+  public void parse_logInCommandWithoutUsername_ExceptionThrown() {
+    
+    try {
+      CommandParser.parse("login");
+    } catch (Exception e) {
+      assertTrue(e instanceof IllegalArgumentException);
+    }
+  }
+
+  @Test
+  public void parse_createAccountCommand_createsCreateAccountCommand() {
+    
+    try {
+      Command command = CommandParser.parse("create_account alan_turing");
+      assertTrue(command instanceof CreateAccountCommand);
+    } catch (Exception e) {
+      assertTrue(false);
+    }
+  }
+
+  @Test
+  public void parse_createAccountCommandWithoutUsername_ExceptionThrown() {
+    
+    try {
+      CommandParser.parse("create_account");
+    } catch (Exception e) {
+      assertTrue(e instanceof IllegalArgumentException);
+    }
+  }
+
+  @Test
+  public void parse_deleteAccountCommand_createsDeleteAccountCommand() {
+    
+    try {
+      Command command = CommandParser.parse("delete_account alan_turing");
+      assertTrue(command instanceof DeleteAccountCommand);
+    } catch (Exception e) {
+      assertTrue(false);
+    }
+  }
+
+  @Test
+  public void parse_deleteAccountCommandWithoutUsername_ExceptionThrown() {
+    
+    try {
+      CommandParser.parse("delete_account");
+    } catch (Exception e) {
+      assertTrue(e instanceof IllegalArgumentException);
+    }
+  }
+
+  @Test
+  public void parse_listAccountsCommand_createsListAccountsCommand() {
+    
+    try {
+      Command command = CommandParser.parse("list_accounts alan_*");
+      assertTrue(command instanceof ListAccountsCommand);
+    } catch (Exception e) {
+      assertTrue(false);
+    }
+  }
+
+  @Test
+  public void parse_listAccountsCommandWithoutPattern_ExceptionThrown() {
+    
+    try {
+      CommandParser.parse("list_accounts");
+    } catch (Exception e) {
+      assertTrue(e instanceof IllegalArgumentException);
+    }
+  }
+
+  @Test
+  public void parse_sendMessageCommand_createsSendMessageCommand() {
+    
+    try {
+      Command command = CommandParser.parse("send alan_turing hello");
+      assertTrue(command instanceof SendMessageCommand);
+    } catch (Exception e) {
+      assertTrue(false);
+    }
+  }
+
+  @Test
+  public void parse_sendMessageCommandWithOnlyOneArgument_ExceptionThrown() {
+    
+    try {
+      CommandParser.parse("send alan_turing");
+    } catch (Exception e) {
+      assertTrue(e instanceof IllegalArgumentException);
+    }
+  }
+
+  @Test
+  public void parse_sendMessageCommandWithoutArguments_ExceptionThrown() {
+    
+    try {
+      CommandParser.parse("send");
+    } catch (Exception e) {
+      assertTrue(e instanceof IllegalArgumentException);
+    }
+  }
+}
