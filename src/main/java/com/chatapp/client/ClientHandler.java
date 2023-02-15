083432574d840798eb64381334a35a5daa7a83eb
Sean Y <seanyang@college.harvard.edu>
2023-02-14 23:56:11 -0500

    Merge

--- src/main/java/com/chatapp/client/ClientHandler.java
+++ src/main/java/com/chatapp/client/ClientHandler.java
@@ -1,5 +1,9 @@
 package com.chatapp.client;
 
+import com.chatapp.Chat;
+import com.chatapp.ChatServiceGrpc;
+import com.chatapp.Chat.CreateAccountRequest;
+import com.chatapp.ChatServiceGrpc.ChatServiceStub;
 import com.chatapp.ChatServiceGrpc.ChatServiceBlockingStub;
 import com.chatapp.client.commands.CreateAccountCommand;
 import com.chatapp.client.commands.DeleteAccountCommand;
@@ -12,6 +16,8 @@ import com.chatapp.client.exceptions.NotLoggedInException;
 import com.chatapp.client.exceptions.UserAlreadyExistsException;
 import com.chatapp.client.exceptions.UserDoesNotExistException;
 
+import com.chatapp.protocol.Exception;
+
 // Helps the Client handle commands from the user because the Client is responsible for the UI logic. Each of the functions handles its command bu sending requests to the server, waiting for a response, then passing the response back to the caller.
 
 // define new exceptions below UserAlreadyExists and UserDoesNotExist
@@ -19,7 +25,27 @@ import com.chatapp.client.exceptions.UserDoesNotExistException;
 
 public class ClientHandler {
   public static void createAccount(CreateAccountCommand command, ChatServiceBlockingStub stub) throws UserAlreadyExistsException, InvalidUsernameException {
-
+    // create stub using channel
+    ChatServiceGrpc.ChatServiceBlockingStub stub = ChatServiceGrpc.newBlockingStub(channel);
+    // create request using command
+    CreateAccountRequest request =
+      Chat.CreateAccountRequest.newBuilder()
+        .setUsername(command.getUsername())
+        .build();
+    // Finally, make the call using the stub
+    Chat.CreateAccountResponse response = 
+      stub.createAccount(request);
+    // handle exceptions
+    if (response.getException() != 0) {
+      switch (com.chatapp.protocol.Exception.fromInt(response.getException())) {
+        case USER_ALREADY_EXISTS:
+          throw new UserAlreadyExistsException("Cannot create account because the username already exists.");
+        case INVALID_USERNAME:
+          throw new InvalidUsernameException("Cannot create account because the username is invalid.");
+        default:
+          break;
+      }
+    }
   }
 
   public static void deleteAccount(DeleteAccountCommand command, ChatServiceBlockingStub stub) throws InvalidUsernameException {
