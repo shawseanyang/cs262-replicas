d80e675a27a167e4f9284a24f0d4402b74b35281
Sean Y <seanyang@college.harvard.edu>
2023-02-17 13:55:54 -0500

    Replace Constant with a class

--- src/main/java/com/chatapp/protocol/Constant.java
+++ src/main/java/com/chatapp/protocol/Constant.java
@@ -1,12 +1,5 @@
 package com.chatapp.protocol;
 
-public enum Constant {
-  // port
-  PORT(8080);
-
-  public final int value;
-
-  private Constant(int value) {
-      this.value = value;
-  }
+public class Constant {
+  public final static int PORT = 8080;
 }
